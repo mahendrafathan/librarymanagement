1297d535779a191af250e0ab1d86fe5bdd0e3042
Julhend <meetredmist@gmail.com>
2021-02-09 16:14:12 +0700

    creating independent model

--- src/main/java/com/glints/librarymanagement/model/Borrowing.java
+++ src/main/java/com/glints/librarymanagement/model/Borrowing.java
@@ -1,5 +1,109 @@
 package com.glints.librarymanagement.model;
 
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "borrowing")
 public class Borrowing {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private String id;
+	// should be join
+	@Column(name = "member_id", nullable = false)
+	private String memberId;
+	// should be join
+	@Column(name = "employee_id", nullable = false)
+	private String employeeId;
+	// should be join
+	@Column(name = "book_id", nullable = false)
+	private String bookId;
+
+	@Column(name = "borrow_date", nullable = false)
+	private Date borrowDate;
+
+	@Column(name = "return_date", nullable = false)
+	private Date returnDate;
+
+	@Column(name = "is_returned", nullable = false)
+	private Boolean isReturned;
+
+	public Borrowing() {
+		super();
+	}
+
+	public Borrowing(String id, String memberId, String employeeId, String bookId, Date borrowDate, Date returnDate,
+			Boolean isReturned) {
+		this.id = id;
+		this.memberId = memberId;
+		this.employeeId = employeeId;
+		this.bookId = bookId;
+		this.borrowDate = borrowDate;
+		this.returnDate = returnDate;
+		this.isReturned = isReturned;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+
+	public String getEmployeeId() {
+		return employeeId;
+	}
+
+	public void setEmployeeId(String employeeId) {
+		this.employeeId = employeeId;
+	}
+
+	public String getBookId() {
+		return bookId;
+	}
+
+	public void setBookId(String bookId) {
+		this.bookId = bookId;
+	}
+
+	public Date getBorrowDate() {
+		return borrowDate;
+	}
+
+	public void setBorrowDate(Date borrowDate) {
+		this.borrowDate = borrowDate;
+	}
+
+	public Date getReturnDate() {
+		return returnDate;
+	}
+
+	public void setReturnDate(Date returnDate) {
+		this.returnDate = returnDate;
+	}
+
+	public Boolean getIsReturned() {
+		return isReturned;
+	}
+
+	public void setIsReturned(Boolean isReturned) {
+		this.isReturned = isReturned;
+	}
 
 }
--- src/main/java/com/glints/librarymanagement/repository/BorrowingRepo.java
+++ src/main/java/com/glints/librarymanagement/repository/BorrowingRepo.java
@@ -1,5 +1,9 @@
 package com.glints.librarymanagement.repository;
 
-public interface BorrowingRepo {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.glints.librarymanagement.model.Borrowing;
+
+public interface BorrowingRepo extends JpaRepository<Borrowing, String> {
 
 }
