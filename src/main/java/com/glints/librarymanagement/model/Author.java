c1eaeefeef07954bd867b98cfd1b0ea52afe2f21
Julhend <meetredmist@gmail.com>
2021-02-11 15:32:06 +0700

    fix merged

--- src/main/java/com/glints/librarymanagement/model/Author.java
+++ src/main/java/com/glints/librarymanagement/model/Author.java
@@ -1,165 +1,135 @@
 package com.glints.librarymanagement.model;
 
-<<<<<<< HEAD
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-import javax.persistence.*;
 import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = "authors")
 public class Author {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private int id;
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
 
-    @Column(name = "firstname", nullable = false)
-    private String firstname;
+	@Column(name = "firstname", nullable = false)
+	private String firstname;
 
-    @Column(name = "surname")
-    private String surname;
+	@Column(name = "surname")
+	private String surname;
 
-    @Column(name = "place_of_birth", nullable = false)
-    private String placeOfBirth;
+	@Column(name = "place_of_birth", nullable = false)
+	private String placeOfBirth;
 
-    @Column(name = "date_of_birth", nullable = false)
-    private Date dateOfBirth;
+	@Column(name = "date_of_birth", nullable = false)
+	private Date dateOfBirth;
 
-    @Column(name = "contact")
-    private String contact;
+	@Column(name = "contact")
+	private String contact;
 
-    @Column(name = "email", nullable = false)
-    private String email;
+	@Column(name = "email", nullable = false)
+	private String email;
 
-    @Column(name = "address")
-    private String address;
+	@Column(name = "address")
+	private String address;
 
-    @Column(name = "isDeleted")
-    private boolean isDeleted;
+	@Column(name = "isDeleted")
+	private boolean isDeleted;
 
 //    @JoinColumn(name = "book_id")
 //    @ManyToMany(targetEntity = Book.class, fetch = FetchType.LAZY)
 //    @JsonIgnore
 //    private Book book;
 
-    public Author() {
-    }
+	public Author() {
+	}
 
-    public Author(String firstname, String surname, String placeOfBirth, Date dateOfBirth, String contact, String email, String address, boolean isDeleted) {
-        this.firstname = firstname;
-        this.surname = surname;
-        this.placeOfBirth = placeOfBirth;
-        this.dateOfBirth = dateOfBirth;
-        this.contact = contact;
-        this.email = email;
-        this.address = address;
-        this.isDeleted = isDeleted;
-    }
+	public Author(String firstname, String surname, String placeOfBirth, Date dateOfBirth, String contact, String email,
+			String address, boolean isDeleted) {
+		this.firstname = firstname;
+		this.surname = surname;
+		this.placeOfBirth = placeOfBirth;
+		this.dateOfBirth = dateOfBirth;
+		this.contact = contact;
+		this.email = email;
+		this.address = address;
+		this.isDeleted = isDeleted;
+	}
 
-    public int getId() {
-        return id;
-    }
+	public int getId() {
+		return id;
+	}
 
-    public void setId(int id) {
-        this.id = id;
-    }
+	public void setId(int id) {
+		this.id = id;
+	}
 
-    public String getFirstname() {
-        return firstname;
-    }
+	public String getFirstname() {
+		return firstname;
+	}
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
-    }
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
 
-    public String getSurname() {
-        return surname;
-    }
+	public String getSurname() {
+		return surname;
+	}
 
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
+	public void setSurname(String surname) {
+		this.surname = surname;
+	}
 
-    public String getPlaceOfBirth() {
-        return placeOfBirth;
-    }
+	public String getPlaceOfBirth() {
+		return placeOfBirth;
+	}
 
-    public void setPlaceOfBirth(String placeOfBirth) {
-        this.placeOfBirth = placeOfBirth;
-    }
+	public void setPlaceOfBirth(String placeOfBirth) {
+		this.placeOfBirth = placeOfBirth;
+	}
 
-    public Date getDateOfBirth() {
-        return dateOfBirth;
-    }
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
 
-    public void setDateOfBirth(Date dateOfBirth) {
-        this.dateOfBirth = dateOfBirth;
-    }
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
 
-    public String getContact() {
-        return contact;
-    }
+	public String getContact() {
+		return contact;
+	}
 
-    public void setContact(String contact) {
-        this.contact = contact;
-    }
+	public void setContact(String contact) {
+		this.contact = contact;
+	}
 
-    public String getEmail() {
-        return email;
-    }
+	public String getEmail() {
+		return email;
+	}
 
-    public void setEmail(String email) {
-        this.email = email;
-    }
+	public void setEmail(String email) {
+		this.email = email;
+	}
 
-    public String getAddress() {
-        return address;
-    }
+	public String getAddress() {
+		return address;
+	}
 
-    public void setAddress(String address) {
-        this.address = address;
-    }
+	public void setAddress(String address) {
+		this.address = address;
+	}
 
-    public boolean isDeleted() {
-        return isDeleted;
-    }
+	public boolean isDeleted() {
+		return isDeleted;
+	}
 
-    public void setDeleted(boolean deleted) {
-        isDeleted = deleted;
-=======
-import javax.persistence.*;
-
-@Entity
-@Table(name = "author")
-public class Author extends Persistence {
-    @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
-    private Long author_id;
-
-    @Column(name = "author_name")
-    private String authorName;
-
-    public Author() {}
-
-    public Author(String author_name) {
-        this.authorName = author_name;
-    }
-
-    public Long getAuthor_id() {
-        return author_id;
-    }
-
-    public void setAuthor_id(Long author_id) {
-        this.author_id = author_id;
-    }
-
-    public String getAuthorName() {
-        return authorName;
-    }
-
-    public void setAuthorName(String authorName) {
-        this.authorName = authorName;
->>>>>>> refs/remotes/origin/jecky-book
-    }
+	public void setDeleted(boolean deleted) {
+		isDeleted = deleted;
+	}
 }
--- src/main/java/com/glints/librarymanagement/model/Persistence.java
+++ src/main/java/com/glints/librarymanagement/model/Persistence.java
@@ -1,10 +1,13 @@
 package com.glints.librarymanagement.model;
 
-<<<<<<< HEAD
-import lombok.AccessLevel;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.EntityListeners;
+import javax.persistence.MappedSuperclass;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 import org.springframework.data.annotation.CreatedBy;
@@ -14,8 +17,10 @@ import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
-import javax.persistence.*;
-import java.util.Date;
+import lombok.AccessLevel;
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 @Getter(AccessLevel.PROTECTED)
 @Setter(AccessLevel.PROTECTED)
@@ -24,122 +29,55 @@ import java.util.Date;
 @MappedSuperclass
 @EntityListeners(AuditingEntityListener.class)
 public class Persistence {
-    @Column(length = 25)
-    @CreatedBy
-    private String createBy;
-
-    @Column
-    @Temporal(TemporalType.TIMESTAMP)
-    @CreatedDate
-    @CreationTimestamp
-    private Date createTime;
-
-    @Column(length = 25)
-    @LastModifiedBy
-    private String updateBy;
-
-    @Column(length = 25)
-    @Temporal(TemporalType.TIMESTAMP)
-    @LastModifiedDate
-    @UpdateTimestamp
-    private Date updateTime;
-
-    public String getCreateBy() {
-        return createBy;
-    }
-
-    public void setCreateBy(String createBy) {
-        this.createBy = createBy;
-    }
-
-    public Date getCreateTime() {
-        return createTime;
-    }
-
-    public void setCreateTime(Date createTime) {
-        this.createTime = createTime;
-    }
-
-    public String getUpdateBy() {
-        return updateBy;
-    }
-
-    public void setUpdateBy(String updateBy) {
-        this.updateBy = updateBy;
-    }
-
-    public Date getUpdateTime() {
-        return updateTime;
-    }
-
-    public void setUpdateTime(Date updateTime) {
-        this.updateTime = updateTime;
-    }
-=======
-import java.util.Date;
-
-import javax.persistence.Column;
-import javax.persistence.EntityListeners;
-import javax.persistence.MappedSuperclass;
-import javax.persistence.Temporal;
-import javax.persistence.TemporalType;
-
-import org.springframework.data.annotation.CreatedBy;
-import org.springframework.data.annotation.CreatedDate;
-import org.springframework.data.annotation.LastModifiedBy;
-import org.springframework.data.annotation.LastModifiedDate;
-import org.springframework.data.jpa.domain.support.AuditingEntityListener;
-
-import com.fasterxml.jackson.annotation.JsonIgnore;
-
-@MappedSuperclass
-@EntityListeners(AuditingEntityListener.class)
-public class Persistence {
-	@Column(length = 50)
-	@CreatedDate
-	@Temporal(TemporalType.TIMESTAMP)
-//	@JsonIgnore
-	private Date createAt;
-	
-	@Column(length = 50)
-	@LastModifiedDate
-	@Temporal(TemporalType.TIMESTAMP)
-//	@JsonIgnore
-	private Date updateAt;
-	
-	@Column(length = 50)
+	@Column(length = 25)
 	@CreatedBy
-	@JsonIgnore
 	private String createBy;
-	
-	@Column(length = 50)
+
+	@Column
+	@Temporal(TemporalType.TIMESTAMP)
+	@CreatedDate
+	@CreationTimestamp
+	private Date createTime;
+
+	@Column(length = 25)
 	@LastModifiedBy
-	@JsonIgnore
 	private String updateBy;
-	
-	public Date getCreateAt() {
-		return createAt;
-	}
-	public void setCreateAt(Date createAt) {
-		this.createAt = createAt;
-	}
-	public Date getUpdateAt() {
-		return updateAt;
-	}
-	public void setUpdateAt(Date updateAt) {
-		this.updateAt = updateAt;
-	}
+
+	@Column(length = 25)
+	@Temporal(TemporalType.TIMESTAMP)
+	@LastModifiedDate
+	@UpdateTimestamp
+	private Date updateTime;
+
 	public String getCreateBy() {
 		return createBy;
 	}
+
 	public void setCreateBy(String createBy) {
 		this.createBy = createBy;
 	}
+
+	public Date getCreateTime() {
+		return createTime;
+	}
+
+	public void setCreateTime(Date createTime) {
+		this.createTime = createTime;
+	}
+
 	public String getUpdateBy() {
 		return updateBy;
 	}
+
 	public void setUpdateBy(String updateBy) {
 		this.updateBy = updateBy;
 	}
->>>>>>> refs/remotes/origin/najib-member-feature
+
+	public Date getUpdateTime() {
+		return updateTime;
+	}
+
+	public void setUpdateTime(Date updateTime) {
+		this.updateTime = updateTime;
+	}
 }
