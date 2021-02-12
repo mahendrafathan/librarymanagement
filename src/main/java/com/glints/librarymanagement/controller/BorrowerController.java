f40bab9266edffa9742dfdd68fd16d92daca4b5f
Julhend <meetredmist@gmail.com>
2021-02-12 13:16:20 +0700

    update status isReturned

--- src/main/java/com/glints/librarymanagement/controller/BorrowerController.java
+++ src/main/java/com/glints/librarymanagement/controller/BorrowerController.java
@@ -5,9 +5,11 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -16,6 +18,7 @@ import com.glints.librarymanagement.model.Book;
 import com.glints.librarymanagement.model.Borrower;
 import com.glints.librarymanagement.model.Member;
 import com.glints.librarymanagement.model.Petugas;
+import com.glints.librarymanagement.payload.AuthorPayload;
 import com.glints.librarymanagement.payload.BorrowerPayload;
 import com.glints.librarymanagement.payload.ErrorResponse;
 import com.glints.librarymanagement.repository.BookRepo;
@@ -95,6 +98,91 @@ public class BorrowerController {
 		}
 
 		return new ResponseEntity<BorrowerPayload>(payload, HttpStatus.CREATED);
+	}
+
+	@DeleteMapping(path = "/delete/{id}", produces = "application/json")
+	public ResponseEntity<?> deleteAuthor(@PathVariable("id") Long id) {
+		Borrower borrower = borrowerRepo.findById(id).orElse(null);
+		if (borrower == null) {
+			return new ResponseEntity<ErrorResponse>(
+					new ErrorResponse("Borrower not found", "Data borrower tidak ditemukan."), HttpStatus.NOT_FOUND);
+		}
+		if (borrower.isDeleted() == true) {
+			return new ResponseEntity<ErrorResponse>(
+					new ErrorResponse("Borrower has deleted.", "Borrower sudah dihapus."), HttpStatus.FORBIDDEN);
+		}
+
+		try {
+			borrower.setDeleted(true);
+			borrowerRepo.save(borrower);
+		} catch (Exception e) {
+			return new ResponseEntity<ErrorResponse>(new ErrorResponse(e.getMessage(), "Request anda gagal"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<String>("Delete borrower success!", HttpStatus.ACCEPTED);
+	}
+
+	@DeleteMapping(path = "/cancel-delete/{id}", produces = "application/json")
+	public ResponseEntity<?> cancelDeleteAuthor(@PathVariable("id") Long id) {
+		Borrower borrower = borrowerRepo.findById(id).orElse(null);
+		if (borrower == null) {
+			return new ResponseEntity<ErrorResponse>(
+					new ErrorResponse("Borrower not found", "Data borrower tidak ditemukan."), HttpStatus.NOT_FOUND);
+		}
+		if (borrower.isDeleted() == false) {
+			return new ResponseEntity<ErrorResponse>(
+					new ErrorResponse("Borrower data is active.", "Data borrower masih tersedia."),
+					HttpStatus.FORBIDDEN);
+		}
+
+		try {
+			borrower.setDeleted(false);
+			borrowerRepo.save(borrower);
+		} catch (Exception e) {
+			return new ResponseEntity<ErrorResponse>(new ErrorResponse(e.getMessage(), "Request anda gagal"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<String>("Undo delete borrower success!", HttpStatus.ACCEPTED);
+	}
+
+	@DeleteMapping(path = "/delete-permanent/{id}", produces = "application/json")
+	public ResponseEntity<?> forceDeleteAuthor(@PathVariable("id") Long id) {
+		Borrower borrower = borrowerRepo.findById(id).orElse(null);
+		if (borrower == null) {
+			return new ResponseEntity<ErrorResponse>(
+					new ErrorResponse("Borrower not found", "Data borrower tidak ditemukan."), HttpStatus.NOT_FOUND);
+		}
+		if (borrower.isDeleted() == false) {
+			return new ResponseEntity<ErrorResponse>(
+					new ErrorResponse("Borrower data is active.", "Data borrower tidak dapat dihapus permanent."),
+					HttpStatus.FORBIDDEN);
+		}
+
+		borrowerRepo.deleteById(id);
+		return new ResponseEntity<String>("Delete borrower data success! borrower deleted permanent!",
+				HttpStatus.ACCEPTED);
+	}
+
+	@PutMapping(path = "/return/status/{id}", consumes = "application/json", produces = "application/json")
+	public ResponseEntity<?> updateIsReturned(@PathVariable("id") Long id, @RequestBody AuthorPayload payload)
+			throws Exception {
+		Borrower existBorrower = borrowerRepo.findById(id).orElse(null);
+		if (existBorrower == null) {
+			return new ResponseEntity<ErrorResponse>(
+					new ErrorResponse("Borrower not found", "Data borrower tidak ditemukan."), HttpStatus.BAD_REQUEST);
+		}
+		try {
+
+			existBorrower.setIsReturned(true);
+			borrowerRepo.save(existBorrower);
+		} catch (Exception e) {
+			return new ResponseEntity<ErrorResponse>(new ErrorResponse(e.getMessage(), "Request anda gagal"),
+					HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+
+		return new ResponseEntity<AuthorPayload>(payload, HttpStatus.ACCEPTED);
 	}
 
 }
