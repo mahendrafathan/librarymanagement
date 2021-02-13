f80c9df4f8c82d431a6759feebc0f9f3cae5f6d6
Julhend <meetredmist@gmail.com>
2021-02-13 13:55:23 +0700

    fix merged

--- src/main/java/com/glints/librarymanagement/repository/BorrowingRepo.java
+++ src/main/java/com/glints/librarymanagement/repository/BorrowingRepo.java
@@ -8,6 +8,6 @@ import org.springframework.data.jpa.repository.Query;
 import com.glints.librarymanagement.model.Borrowing;
 
 public interface BorrowingRepo extends JpaRepository<Borrowing, Long> {
-	@Query(value = "SELECT * FROM borrowers LEFT JOIN employees ON employees.id=borrowers.employee_id LEFT JOIN books  ON books.book_id = borrowers.book_id LEFT JOIN members  ON members.id = borrowers.member_id;", nativeQuery = true)
+	@Query(value = "SELECT * FROM borrowing LEFT JOIN employees ON employees.id=borrowers.employee_id LEFT JOIN books  ON books.book_id = borrowers.book_id LEFT JOIN members  ON members.id = borrowers.member_id;", nativeQuery = true)
 	List<Borrowing> findAllJoin();
 }
