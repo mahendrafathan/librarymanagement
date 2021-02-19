15f6ee62e6f4ef18f413abc7125b173ee7052bd0
Agus Priyatin <aguspriyatin80>
2021-02-20 01:32:12 +0700

    login with password normally

--- src/main/java/com/glints/librarymanagement/config/SecurityConfig.java
+++ src/main/java/com/glints/librarymanagement/config/SecurityConfig.java
@@ -12,6 +12,7 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -31,9 +32,12 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         auth.userDetailsService(userDetailsService);
     }
     @Bean
-    public PasswordEncoder passwordEncoder(){
-        return NoOpPasswordEncoder.getInstance();
-    }
+//  public PasswordEncoder passwordEncoder(){
+//      return NoOpPasswordEncoder.getInstance();
+//  }
+	  public PasswordEncoder passwordEncoder() {
+	  	return new BCryptPasswordEncoder();
+	  }
 
     @Bean(name = BeanIds.AUTHENTICATION_MANAGER)
     @Override
